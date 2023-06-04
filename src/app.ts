0850ca6386df0d1f11f90f3116855767c27373b9
Showkot <showkotus@gmail.com>
2023-06-04 06:27:36 +0600

    final final

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,12 @@ import express, { Application, Request, Response, urlencoded } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-
 app.use(cors)
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
-
-app.get('/', (req: Request, res: any) => {
-    res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
 })
 
-
-
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,15 @@ import app from './app'
 import config from './config/index'
 
 async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database connected successfully`)
 
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database connected successfully`)
-
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log('Failed to connect Database', err)
-    }
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Failed to connect Database', err)
+  }
 }
-bootstrap();
+bootstrap()
